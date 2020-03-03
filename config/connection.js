f676ee027cca19bac30eafbbfde3de4be7f08429
anushaodilzoda <51028662+anushaodilzoda@users.noreply.github.com>
2020-03-03 18:44:28 -0500

    added comments

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 
 }
 
-// Make connection.
+// Make connection
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
