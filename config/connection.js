967568d319ca6d5736989565d7e715ca485fa041
anushaodilzoda <51028662+anushaodilzoda@users.noreply.github.com>
2020-03-09 20:14:50 -0400

    ad

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
 } else {
   // mysql://b92d7740db9d45:eb1c4b3b@us-cdbr-iron-east-04.cleardb.net/heroku_ce95f5c76ca4d26?reconnect=true
 var connection = mysql.createConnection({
-  host: "lus-cdbr-iron-east-04.cleardb.net",
+  host: "localhost",
   port: 3306,
-  user: "b92d7740db9d45",
-  password: "eb1c4b3b",
-  database: "heroku_ce95f5c76ca4d26"
+  user: "root",
+  password: "Anushaj0n",
+  database: "burgers_db"
 });
 
 }
